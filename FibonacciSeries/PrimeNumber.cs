af6f8a49314bc0d871a47de43cf33bbe8fbb64b5
shivrjpatil <shivrajp972@gmail.com>
2022-11-02 20:28:19 +0530

    [shiv]add:primenumber

---
+++ FibonacciSeries/PrimeNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class PrimeNumber
+    {
+        public void CheckPrime(int num)
+        {
+            int count = 0;
+            for(int i= 1; i <= num; i++)
+            {
+                if(num%i==0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine("num is prinmeNumber");
+            else
+                Console.WriteLine("Num is not prime number");
+        }
+    }
+}
