b980f4cc6f706cbeb61223fede7a3791cee9a45d
shivrjpatil <shivrajp972@gmail.com>
2022-11-02 20:49:21 +0530

    [shiv]add:reversenumber

---
+++ FibonacciSeries/ReverseNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class ReverseNumber
+    {
+        public void Reverse(int num)
+        {
+            int Reverse = 0;
+            while(num>0)
+            {
+                int Remainder = num % 10;
+                Reverse = Reverse *10+ Remainder;
+                num /= 10;
+            }
+            Console.WriteLine(Reverse);
+        }
+    }
+}
