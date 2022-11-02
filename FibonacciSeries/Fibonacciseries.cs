230483da7b71013e325c9bb072327c8b4bca72be
shivrjpatil <shivrajp972@gmail.com>
2022-11-02 20:22:47 +0530

    [shiv]add:fibonacciseries

---
+++ FibonacciSeries/Fibonacciseries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BasicCoreprogram
+{
+    public class FibonacciSeries
+    {
+        public void Print(int num)
+        {
+            int firstNum = 0, SecondNum = 1, ThirdNum = 0;
+            Console.WriteLine(firstNum + "\n " + SecondNum);
+            for (int i = 0; i < num; i++)
+            {
+                ThirdNum = firstNum + SecondNum;
+                firstNum = SecondNum;
+                SecondNum = ThirdNum;
+                Console.WriteLine(ThirdNum);
+
+            }
+        }
+    }
+}
