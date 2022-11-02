d31a72fdcff76313fbdf97072dfb18584e4fde66
shivrjpatil <shivrajp972@gmail.com>
2022-11-02 20:54:08 +0530

    [shiv]:basiccoreprogram

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,13 @@
+﻿using BasicCoreprogram;
+using System;
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseNumber prime = new ReverseNumber();
+             prime.Reverse(1234);
+        }
+    }
+}
